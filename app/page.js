d76dc8d24368fd685305b4af36ffd608f1d51844
LoGiSTeRr <danikbor160@Gmail.com>
2024-04-17 23:12:47 +0400

    encryption implemented

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
+import Encryption from "@/components/Encryption";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
-      <div className="flex flex-col gap-20 h-[850px]">
+      <div className="flex flex-col gap-20 h-auto w-full">
           <Hero/>
           <Skills/>
+          <Encryption/>
       </div>
     </main>
   );
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -6,7 +6,7 @@ import SkillText from "@/components/SkillText";
 const Skills = () => {
     return (
         <section id="skills"
-                 className="flex flex-col items-center justify-center gap-3 h-full relative pb-80 py-20"
+                 className="flex flex-col items-center justify-center gap-3 h-auto relative pb-80 py-20"
                  style={{transform: "scale(0.9)"}}>
             <SkillText/>
             <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
