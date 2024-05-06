5a4f4b0e116712e343a84e2455bc7c8eae4f6827
LoGiSTeRr <danikbor160@Gmail.com>
2024-05-06 21:15:08 +0400

    plausible

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,28 @@
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/components/StarBackground";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import PlausibleProvider from "next-plausible";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata = {
-  title: "Space Website",
-  description: "Done by LoGiSTeRr",
+    title: "Space Website",
+    description: "Done by LoGiSTeRr",
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
-      <StarsCanvas/>
-      <Navbar/>
-      {children}
-      <Footer/>
-      </body>
-    </html>
-  );
+export default function RootLayout({children}) {
+    return (
+        <PlausibleProvider domain="space-website-hazel.vercel.app">
+            <html lang="en">
+            <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
+            <StarsCanvas/>
+            <Navbar/>
+            {children}
+            <Footer/>
+            </body>
+            </html>
+        </PlausibleProvider>
+    );
 }
--- app/page.js
+++ app/page.js
@@ -2,19 +2,17 @@ import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
 import Encryption from "@/components/Encryption";
 import Projects from "@/components/Projects";
-import PlausibleProvider from 'next-plausible'
 
 export default function Home() {
     return (
-        <PlausibleProvider domain="space-website-hazel.vercel.app">
-            <main className="h-full w-full">
-                <div className="flex flex-col gap-20 h-auto w-full">
-                    <Hero/>
-                    <Skills/>
-                    <Encryption/>
-                    <Projects/>
-                </div>
-            </main>
-        </PlausibleProvider>
+
+        <main className="h-full w-full">
+            <div className="flex flex-col gap-20 h-auto w-full">
+                <Hero/>
+                <Skills/>
+                <Encryption/>
+                <Projects/>
+            </div>
+        </main>
     );
 }
