987a4e88692d68fc9ced207326de26f0f755a7fd
LoGiSTeRr <danikbor160@Gmail.com>
2024-05-06 20:46:20 +0400

    plausible

--- app/page.js
+++ app/page.js
@@ -2,16 +2,19 @@ import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
 import Encryption from "@/components/Encryption";
 import Projects from "@/components/Projects";
+import PlausibleProvider from 'next-plausible'
 
 export default function Home() {
-  return (
-    <main className="h-full w-full">
-      <div className="flex flex-col gap-20 h-auto w-full">
-          <Hero/>
-          <Skills/>
-          <Encryption/>
-          <Projects/>
-      </div>
-    </main>
-  );
+    return (
+        <PlausibleProvider domain="space-website-hazel.vercel.app">
+            <main className="h-full w-full">
+                <div className="flex flex-col gap-20 h-auto w-full">
+                    <Hero/>
+                    <Skills/>
+                    <Encryption/>
+                    <Projects/>
+                </div>
+            </main>
+        </PlausibleProvider>
+    );
 }
